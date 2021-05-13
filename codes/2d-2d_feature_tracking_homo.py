97acb36aa35421d1fcff8b8f770bc7e911cd4d6c
parees pathak <64767345+pareespathak@users.noreply.github.com>
2021-05-14 00:47:15 +0530

    Update 2d-2d_feature_tracking_homo.py

--- codes/2d-2d_feature_tracking_homo.py
+++ codes/2d-2d_feature_tracking_homo.py
@@ -1,3 +1,4 @@
+#importing packages 
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
@@ -6,59 +7,60 @@ import os
 
 #importing images from folder
 
-path = 'C:\\aa\\visual_odometry\\project\\KITTI_sample'
-p = os.path.join(path, "images")
+path = 'C:\\aa\\visual_odometry\\project\\KITTI_sample'         # path of the image folder
+p = os.path.join(path, "images")                                # image folder name
 images = []
 for img in os.listdir(p):
     image = cv.imread(os.path.join(p, img), cv.IMREAD_GRAYSCALE)
     images.append(image)
+ 
+#Calibration Matrix (KITTI datasets )
+k =np.array([[7.188560000000e+02, 0.000000000000e+00, 6.071928000000e+02],
+             [0.000000000000e+00, 7.188560000000e+02, 1.852157000000e+02],
+             [0.000000000000e+00, 0.000000000000e+00, 1.000000000000e+00]])
 
 '''
-cap = cv.VideoCapture('C:\\aa\\visual_odometry\\project\\L_shaped_path.avi')
+#importing video as image frames
+
+cap = cv.VideoCapture('C:\\aa\\visual_odometry\\project\\L_shaped_path.avi') #video location,video name.format
 ret, old_frame = cap.read()
 images = []
 i =0
+
 while(i<=550):
     ret, old_frame = cap.read()
     image = cv.cvtColor(old_frame, cv.COLOR_BGR2GRAY)
-    cv.imshow('image',image)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
-    #print(image.shape)
     images.append(image)
     i = i+1
-cap.release()
-cv.destroyAllWindows()
-
-print(len(images))
-
+    
+# calibration matrix for video dataset 
 k = np.array([[518.56666108, 0., 329.45801792],
     [0., 518.80466479, 237.05589955],
     [  0., 0., 1.]])
 '''
-#Calibration Matrix
-k =np.array([[7.188560000000e+02, 0.000000000000e+00, 6.071928000000e+02],
-             [0.000000000000e+00, 7.188560000000e+02, 1.852157000000e+02],
-             [0.000000000000e+00, 0.000000000000e+00, 1.000000000000e+00]])
 
 # calcOpticalFlowPyrLK function parameters
 lk_params = dict( winSize  = (15,15),
               maxLevel = 5,
               criteria = (cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03),minEigThreshold = 1e-4)
 
+# Shi-Tomasi corners detector are used 
+# Key point extraction
 def corners(img):
     corners = cv.goodFeaturesToTrack(img, mask = None, maxCorners = 400, qualityLevel = 0.01, minDistance = 7, blockSize = 3, useHarrisDetector = False, k = 0.04  )
     #print(corners.shape)
     return corners
 
-
+#tracking using Kanade-Lucas-Tomasi Optical flow tracker
 def track_features(img1, img2, corners):
     p1, st, err= cv.calcOpticalFlowPyrLK(img1, img2, corners,None,**lk_params)
-    p1 = p1[st == 1]
+    p1 = p1[st == 1]                       #selecting good points
     corners = corners[st == 1]
     return p1,corners
 
-# triangulation
+# triangulation for 3d point cloud estimation 
 def triangulaion(R,t,pt1,pt2,k):
     # projection matrix
     pr = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0]])
@@ -94,15 +96,18 @@ def scale(O_cloud,N_cloud):
 pts1 = corners(images[0])
 c1 = pts1
 pts2,pts1 = track_features(images[0],images[1],pts1)
+#Essential Matrix with Outlier rejection using RANSAC
 E, mask = cv.findEssentialMat(pts2,pts1,k,cv.RANSAC, prob=0.999,threshold = 0.4, mask=None)
 # We select only inlier points
 pts1 = pts1[mask.ravel()==1]
 pts2 = pts2[mask.ravel()==1]
 #Obtain rotation and translation for the essential matrix
-# taking initial rotmat = I and translation mat = 000
+# taking initial rotation matrix as identity = I and translation mat = [0,0,0]
 r_init = np.array([[1,0,0],[0,1,0],[0,0,1]])
 t_init = np.array([[0],[0],[0]])
+# relative rotation and translation between frames
 retval,R,t,mask=cv.recoverPose(E,pts1,pts2,k)
+#point cloud estimation
 n_cloud = triangulaion(r_init,t_init,pts1,pts2,k)
 trans = np.dot(R,t)
 rotation = R
@@ -136,17 +141,21 @@ for j in range(1,150):
     z1 = trans[2]
     x.append(x1)
     z.append(z1)
+    #if occlusion occurs we repeat the process
     if pts2.shape[0] <= 350:                          # for KITTI_sample
     #if pts2.shape[0] <= 395:                         # for video
         c1 = corners(images[j])
 
 # ground truth using pose doc
-ground_truth = np.loadtxt('C:\\aa\\visual_odometry\\project\\KITTI_sample\\poses.txt')
+ground_truth = np.loadtxt('C:\\aa\\visual_odometry\\project\\KITTI_sample\\poses.txt')   #path of ground truth 
 x_truth=[]
 z_truth=[]
+
 for i in range(150):
     x_truth.append(ground_truth[i,3])
     z_truth.append(ground_truth[i,11])
+
+# Results and trajectory
 plt.plot(x_truth,z_truth, label = "ground_truth")
 plt.plot(x,z,color='green',label = "plotted trajectory")
 #plt.scatter(x,z,c='green',s=3)
@@ -155,16 +164,3 @@ plt.ylabel("z")
 plt.title("Results: 2d_2d_tracking_KITTI_sample_Dataset")
 plt.legend()
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-#cv.destroyAllWindows()
