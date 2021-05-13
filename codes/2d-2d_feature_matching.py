a5016d132899fc474652507b9b53608d7992755b
parees pathak <64767345+pareespathak@users.noreply.github.com>
2021-05-14 00:31:59 +0530

    Update 2d-2d_feature_matching.py

--- codes/2d-2d_feature_matching.py
+++ codes/2d-2d_feature_matching.py
@@ -1,3 +1,4 @@
+#importing packages 
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
@@ -6,44 +7,50 @@ import os
 '''
 importing images from folder
 '''
-'''
-path = 'C:\\aa\\visual_odometry\\project\\KITTI_sample'
-p = os.path.join(path, "images")
+
+path = 'C:\\aa\\visual_odometry\\project\\KITTI_sample'      # path of the image folder
+p = os.path.join(path, "images")                             # image folder name
 images = []
 for img in os.listdir(p):
     image = cv.imread(os.path.join(p, img), cv.IMREAD_GRAYSCALE)
     images.append(image)
-#Calibration Matrix
+
+#Calibration Matrix for KITTI sample
 k =np.array([[7.188560000000e+02, 0.000000000000e+00, 6.071928000000e+02],
              [0.000000000000e+00, 7.188560000000e+02, 1.852157000000e+02],
              [0.000000000000e+00, 0.000000000000e+00, 1.000000000000e+00]])
+
+
 '''
-cap = cv.VideoCapture('C:\\aa\\visual_odometry\\project\\L_shaped_path.avi')
+#importing video as image frames
+
+cap = cv.VideoCapture('C:\\aa\\visual_odometry\\project\\L_shaped_path.avi')   #folder location and video name.format
 ret, old_frame = cap.read()
 images = []
 i =0
 while(i<=550):
     ret, old_frame = cap.read()
     image = cv.cvtColor(old_frame, cv.COLOR_BGR2GRAY)
-    cv.imshow('image',image)
+    #cv.imshow('image',image)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
-    #print(image.shape)
     images.append(image)
     i = i+1
-cap.release()
-cv.destroyAllWindows()
+#cap.release()
+#cv.destroyAllWindows()
+#print(len(images))
 
-print(len(images))
+#calibration matrix for video camera (from Zhang's method)
 
 k = np.array([[518.56666108, 0., 329.45801792],
     [0., 518.80466479, 237.05589955],
     [  0., 0., 1.]])
+'''
 
-# tracking features
+# tracking features using SIFT 
 def track_features(img1,img2):
     sift = cv.SIFT_create()
-    # find the keypoints and descriptors with SIFT
+    # find the keypoints and descriptors with SIFT 
     kp1, des1 = sift.detectAndCompute(img1,None)
     kp2, des2 = sift.detectAndCompute(img2,None)
     # FLANN parameters
@@ -58,7 +65,7 @@ def track_features(img1,img2):
     matches = flann.knnMatch(des1,des2,k=2)
     pts1 = []
     pts2 = []
-    # ratio test as per Lowe's paper
+    # ratio test as per Lowe's paper to estimate correspondence 
     for i,(m,n) in enumerate(matches):
         if m.distance < 0.7*n.distance:
             pts2.append(kp2[m.trainIdx].pt)
@@ -68,7 +75,8 @@ def track_features(img1,img2):
     pts2 = np.float32(pts2)
     #print(pts1.shape)
     return pts1,pts2
-# triangulation
+
+# triangulation between image frames to estimate 3d point cloud 
 def triangulaion(R,t,pt1,pt2,k):
     # projection matrix
     pr = np.array([[1,0,0,0],[0,1,0,0],[0,0,1,0]])
@@ -85,8 +93,9 @@ def triangulaion(R,t,pt1,pt2,k):
     cloud = cloud[:4,:]
     return cloud
 
+# relative scale calculation 
 def scale(O_cloud,N_cloud):
-    siz = min(O_cloud.shape[1],N_cloud.shape[1])
+    siz = min(O_cloud.shape[1],N_cloud.shape[1]) 
     o_c = np.zeros((3,siz))
     n_c = np.zeros((3,siz))
     o_c = O_cloud[:,:siz]
@@ -99,21 +108,23 @@ def scale(O_cloud,N_cloud):
     # taking median along the row (norm)
     scale = np.median(scale)
     return scale
+
 ###### for first two images
 pts1,pts2 = track_features(images[0],images[1])
+# Essential matrix with RANSAC outlier rejection
 E, mask = cv.findEssentialMat(pts2,pts1,k,cv.RANSAC, prob=0.999,threshold = 0.4, mask=None)
 # We select only inlier points
 pts1 = pts1[mask.ravel()==1]
 pts2 = pts2[mask.ravel()==1]
 #Obtain rotation and translation for the essential matrix
-# taking initial rotmat = I and translation mat = 000
+# taking initial rotation matrix as identity matrix = I and translation mat = [0,0,0]
 r_init = np.array([[1,0,0],[0,1,0],[0,0,1]])
 t_init = np.array([[0],[0],[0]])
 retval,R,t,mask=cv.recoverPose(E,pts1,pts2,k)
 n_cloud = triangulaion(r_init,t_init,pts1,pts2,k)
 trans = np.dot(R,t)
 rotation = R
-x1 = trans[0]    # taking x and z coordinates for graph
+x1 = trans[0]    # x and z coordinates for graph
 z1 = trans[2]
 x = []
 y = []
@@ -122,9 +133,10 @@ x.append(x1)
 z.append(z1)
 # multiple frames
 for i in range(1,225):
-    j = 2*i
+    #j = 2*i             #for skipping some frames in video datasets            
     o_cloud = n_cloud
-    pts1,pts2 = track_features(images[j],images[j+1])
+    pts1,pts2 = track_features(images[i],images[i+1])
+    #Essential matrix estimation 
     E, mask = cv.findEssentialMat(pts2,pts1,k,cv.RANSAC, prob=0.999,threshold = 0.4 ,mask=None)
     # We select only inlier points
     pts1 = pts1[mask.ravel()==1]
@@ -144,16 +156,19 @@ for i in range(1,225):
 
 x_truth = []
 z_truth = []
+
 # ground truth using pose doc
-ground_truth = np.loadtxt('C:\\aa\\visual_odometry\\project\\KITTI_sample\\poses.txt')
+ground_truth = np.loadtxt('C:\\aa\\visual_odometry\\project\\KITTI_sample\\poses.txt')  #path of ground truth 
 x_truth=[]
 z_truth=[]
 for i in range(150):
     x_truth.append(ground_truth[i,3])
     z_truth.append(ground_truth[i,11])
-#plt.plot(x_truth,z_truth, label = "ground_truth")
-#plt.plot(x,z,color='green',label = "plotted trajectory")
-plt.scatter(x,z,c='green',s=4)
+
+# Results and trajectory 
+plt.plot(x_truth,z_truth, label = "ground_truth")
+plt.plot(x,z,color='green',label = "plotted trajectory")
+#plt.scatter(x,z,c='green',s=4)
 plt.legend()
 plt.xlabel("y")
 plt.ylabel("z")
